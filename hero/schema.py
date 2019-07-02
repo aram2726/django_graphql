1bf4087c7c618c10f5c2bc550a25920a88c6dc42
aram2726 <aramharoyan@gmail.com>
2019-07-02 10:45:15 +0400

    add retrieve

--- hero/schema.py
+++ hero/schema.py
@@ -12,10 +12,21 @@ class HeroType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     heroes = graphene.List(HeroType)
+    hero = graphene.Field(HeroType, id=graphene.Int())
+
     def resolve_heroes(self, info, **kwargs):
         return Hero.objects.all()
 
     def resolve_movie(self, info, **kwargs):
         return MovieType()
+
+    def resolve_hero(self, info, **kwargs):
+        hero_id = kwargs.get('id')
+        if hero_id:
+            try:
+                return Hero.objects.get(pk=hero_id)
+            except Hero.DoesNotExist:
+                return None
+
 
 schema = graphene.Schema(query=Query)
--- movie/schema.py
+++ movie/schema.py
@@ -11,9 +11,17 @@ class MovieType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     movies = graphene.List(MovieType)
+    movie = graphene.Field(MovieType, id=graphene.Int())
 
     def resolve_movies(self, info, **kwargs):
         return Movie.objects.all()
+
+    def resolve_movie(self, info, **kwargs):
+        movie_id = kwargs.get("id")
+        try:
+            return Movie.objects.get(pk=movie_id)
+        except Movie.DoesNotExist:
+            return None
 
 
 schema = graphene.Schema(query=Query)
